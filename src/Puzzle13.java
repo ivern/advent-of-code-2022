4522cd8baa00e7a60174bbe039923615d6da399a
Javier Fernandez-Ivern <ivern@netflix.com>
2023-12-08 14:37:02 -0600

    Day 7 solutions

---
+++ src/Puzzle13.java
@@ -0,0 +1,101 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SuppressWarnings("unused")
+public class Puzzle13 {
+
+    public int solve() throws IOException {
+        var lines = Files.readAllLines(Paths.get("./data/day7.txt"));
+
+        int lineNum = 0;
+        List<Directory> directories = new ArrayList<>();
+        Directory root = new Directory("/");
+        Directory current = root;
+
+        while (lineNum < lines.size()) {
+            var line = lines.get(lineNum++);
+            if (line.startsWith("$ cd")) {
+                var target = line.substring("$ cd ".length());
+                if (target.equals("/")) {
+                    current = root;
+                } else if (target.equals("..")) {
+                    current = current.parent;
+                } else {
+                    if (!current.entries.containsKey(target)) {
+                        var directory = new Directory(target);
+                        current.add(directory);
+                        directories.add(directory);
+                    }
+                    current = (Directory) current.entries.get(target);
+                }
+            } else if (line.startsWith("$ ls")) {
+                while (lineNum < lines.size() && !lines.get(lineNum).startsWith("$")) {
+                    String[] parts = lines.get(lineNum++).split(" ");
+                    if (parts[0].trim().equals("dir")) {
+                        if (!current.entries.containsKey(parts[1].trim())) {
+                            var directory = new Directory(parts[1].trim());
+                            current.add(directory);
+                            directories.add(directory);
+                        }
+                    } else {
+                        current.add(new File(parts[1].trim(), Integer.parseInt(parts[0].trim())));
+                    }
+                }
+            }
+        }
+
+        return directories.stream().mapToInt(Node::size).filter(n -> n <= 100000).sum();
+    }
+
+    private abstract class Node {
+        protected final String name;
+
+        Node(String name) {
+            this.name = name;
+        }
+
+        abstract int size();
+    }
+
+    private class Directory extends Node {
+        Map<String, Node> entries;
+        Directory parent;
+
+        public Directory(String name) {
+            super(name);
+            this.entries = new HashMap<>();
+        }
+
+        @Override
+        int size() {
+            return entries.values().stream().mapToInt(Node::size).sum();
+        }
+
+        void add(Node n) {
+            if (n instanceof Directory d) {
+                d.parent = this;
+            }
+            entries.put(n.name, n);
+        }
+    }
+
+    private class File extends Node {
+        protected final int size;
+
+        public File(String name, int size) {
+            super(name);
+            this.size = size;
+        }
+
+        @Override
+        int size() {
+            return size;
+        }
+    }
+
+}
